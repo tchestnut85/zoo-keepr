e8588271c7663848b726d55fa5b205be8fe7f449
Tom Chestnut <tchestnut85@gmail.com>
2020-10-12 20:39:56 -0400

    added function for get ID route

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const QueryString = require('qs');
 const PORT = process.env.PORT || 3001;
 const app = express();
 const { animals } = require('./data/animals');
@@ -45,12 +44,26 @@ function filterByQuery(query, animalsArray) {
     return filteredResults;
 }
 
-app.get('/api/animals', (req, res) => {
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+}
+
+app.get('/api/animals/', (req, res) => {
     let results = animals;
     if (req.query) {
         results = filterByQuery(req.query, results);
     }
     res.json(results);
+});
+
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+        res.json(result);
+    } else {
+        res.send(404);
+    }
 });
 
 app.listen(PORT, () => {
